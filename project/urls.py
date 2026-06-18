bc6e9479a7ab1d349d821440bd2c4197858f571b
srinu <sampathisrinu03@gmail.com>
2026-06-18 14:19:56 +0530

    feat: create project structure

---
+++ project/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework import routers
+
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # Redirect bare /api/ to Swagger docs; other /api/* routes are handled below
+    path("api/", RedirectView.as_view(url="/api/docs/", permanent=False)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+]
